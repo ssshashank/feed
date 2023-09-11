7a0760cb996e27554c230fd7a0f003c16b8f64e8
Shekhar S <shekharshashank97@gmail.com>
2023-09-11 19:27:07 +0530

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -3,62 +3,65 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
-  output: {
-    publicPath: "http://localhost:3002/",
-  },
+	output: {
+		publicPath:
+			argv.mode === "development"
+				? "http://localhost:3002/"
+				: "https://breeze-feed.vercel.app/",
+	},
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+	},
 
-  devServer: {
-    port: 3002,
-    historyApiFallback: true,
-  },
+	devServer: {
+		port: 3002,
+		historyApiFallback: true,
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.m?js/,
+				type: "javascript/auto",
+				resolve: {
+					fullySpecified: false,
+				},
+			},
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(ts|tsx|js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "feed",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {},
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "feed",
+			filename: "remoteEntry.js",
+			remotes: {},
+			exposes: {},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: deps["react-dom"],
+				},
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+		}),
+	],
 });
